652b12a96f37863deefd815257d9bc5f641a6373
Firaol-A <firaolahmedu@gmail.com>
2025-09-11 09:56:18 -0600

    completed basic shopping list

---
+++ app/week-3/page.js
@@ -0,0 +1,9 @@
+import ItemList from "@/app/week-3/item-list";
+export default function Page(){
+    return(
+        <main>
+            <h1 className="text-4xl font-bold text-center font-mono">Shopping List</h1>
+            <ItemList/>
+        </main>
+    )
+}
