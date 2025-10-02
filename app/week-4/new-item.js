11be0fb2c927db877f678f1be47535390ee3787d
Firaol-A <firaolahmedu@gmail.com>
2025-10-02 08:03:19 -0600

    completed week 4

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -4,14 +4,20 @@ import {useState} from "react";
 export default function Quantity(){
     const [quantity, setQuantity] = useState(1);
     const increment = () => {
-        if (quantity < 20){
+        if (quantity >= 1 && quantity < 20) {
             setQuantity(quantity + 1);
         }
     }
     const decrement = () => {
-        if (quantity === 20){
+        if (quantity <= 20 && quantity > 1) {
             setQuantity(quantity - 1);
         }
     }
+    return (
+        <div className="flex mx-auto border w-1/2 h-24 rounded-md">
+            <p className="text-5xl text-center border h-1/2 w-1/4 m-2 ml-10 mt-6 rounded-md">{quantity}</p>
+            <button onClick={decrement} className={`text-4xl font-bold border h-1/2 w-1/6 ml-6 mt-6 rounded-sm text-white ${quantity === 1 ? "cursor-not-allowed bg-gray-300" : "bg-cyan-600"}`}>-</button>
+            <button onClick={increment} className={`text-4xl font-bold border h-1/2 w-1/6 ml-6 mt-6 rounded-sm text-white ${quantity === 20 ? "cursor-not-allowed bg-gray-300" : "bg-cyan-600"}`}>+</button>
+        </div>
+    )
 }
-
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,12 +1,10 @@
-
+import Quantity from './new-item';
 export default function Page(){
     return (
         <main>
-            <h1 className="text-4xl font-bold text-center font-mono">Week 4</h1>
+            <h1 className="text-4xl font-bold text-center font-mono mb-4">Week 4</h1>
             <section>
-                <p className="text-2xl">Current Quantity Value: </p>
-                <button className="text-xl border rounded-4xl mr-3 pl-10 pr-10 hover:drop-shadow-md">+</button>
-                <button className="text-xl border rounded-4xl mr-3 pl-10 pr-10">-</button>
+                <Quantity />
             </section>
         </main>
     )
