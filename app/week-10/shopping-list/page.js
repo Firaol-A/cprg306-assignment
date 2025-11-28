8ee72beef583caec1888f89ef42f0381cf3f2b0c
FIraol-A <firaol.ahmed@edu.sait.ca>
2025-11-28 15:22:03 -0700

    called loadItems in useEffect: wasnt loading shopping list

--- app/week-10/shopping-list/page.js
+++ app/week-10/shopping-list/page.js
@@ -40,7 +40,7 @@ export default function Page(){
                 setItems(fetchedItems);
             }
         };
-        // loadItems();
+        loadItems();
     }, [user]);
     return(
         <main>
